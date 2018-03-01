48b8becf80ef08d6da7e744c1fe782742e768025
NikitaBalabanov <36954997+NikitaBalabanov@users.noreply.github.com>
2018-03-01 14:32:33 +0300

    Upload

---
+++ WorkAll/Work 3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum all counts before 1000, multiple 3 or 5 ");
+            int sum = 0;
+            int sum1 = 0;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < 1000; i+=3)
+            {
+                sum += i;
+                if ( i % 5 == 0)  
+                {
+                    sum1 += i;
+                }
+            }
+            for (int i = 0; i < 1000; i += 5)
+            {
+                sum += i;
+            }
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed.TotalMilliseconds);
+            watch.Reset();
+            sum -= sum1;
+
+            watch.Start();
+            var sum2 = 0;
+            for (int i = 0; i < 1000; i ++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum2 += i;
+                }
+            }
+
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed.TotalMilliseconds);
+            Console.WriteLine($"Sum = {sum}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WorkAll/Work 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write N");
+            int N = int.Parse(Console.ReadLine());
+            int N1 = N;
+            for (int i = 0; i < N; i++)
+            {
+                string result = new string(' ', N1-1);
+                Console.Write(result);
+                N1--;
+                string result1 = new string('*', i*2+1);
+                Console.Write(result1);
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
